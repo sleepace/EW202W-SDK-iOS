5ff9ab9a0356ed4f4c038ceb5cb29dbc2b6f9e1f
Michael <micheal@sleepace.net>
2020-06-03 11:08:09 +0800

    文档和SDK

---
+++ Framework/EW202W.framework/Headers/SLPEW202WDef.h
@@ -0,0 +1,14 @@
+//
+//  SLPEW202WDef.h
+//  EW202W
+//
+//  Created by Michael on 2020/5/11.
+//  Copyright © 2020 Martin. All rights reserved.
+//
+
+#ifndef SLPEW202WDef_h
+#define SLPEW202WDef_h
+
+#define kNotificationNameLanEW202WMusicPlayProgressChanged @"kNotificationNameLanEW202WMusicPlayProgressChanged"//EW202W的音乐播放进度
+
+#endif /* SLPEW202WDef_h */
