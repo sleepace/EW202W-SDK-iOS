161f1a2672ab98bdb7494877f5db5e0574785960
Michael <micheal@sleepace.net>
2020-06-11 11:00:12 +0800

    SDK和文档更新

--- EW202Wdemo/libs/SLPTCP.framework/Headers/SLPLTcpDef.h
+++ EW202Wdemo/libs/SLPTCP.framework/Headers/SLPLTcpDef.h
@@ -11,6 +11,6 @@
 
 #define kNotificationNameLTCPDisconnected @"kNotificationNameLTCPDisconnected"//长连接断开了
 #define kNotificationNameLTCPConnected @"kNotificationNameLTCPConnected"//长连接连接上了
-#define kNotificationNameNoxUpdateRateChanged @"kNotificationNameNoxUpdateRateChanged" //nox升级进度
+#define kNotificationNameUpdateRateChanged @"kNotificationNameUpdateRateChanged" //升级进度发生了变化
 #define kNotificationNameEW202WWorkModeChanged @"kNotificationNameEW202WWorkModeChanged" //EW202W的工作状态发生了变化
 #endif /* SLPLTcpDef_h */
